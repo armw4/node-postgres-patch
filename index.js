6f7f8544f6cc33d27beb9fc5a9051107904af90e
Antwan Wimberly <antwan.wimberly@gmail.com>
2016-05-05 23:27:52 -0700

    add baseline implementation

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
+var pick = require('lodash.pick')
+var decamelize = require('decamelize')
+var array = require('make-array')
+var PatchError = require('./errors').PatchError
+
 module.exports = function () {
-  return 'it was written'
+  var safeObject = pick.apply(null, array(arguments))
+  var keys = Object.keys(safeObject)
+
+  if (!keys.length) {
+    throw new PatchError('expected an object with at least one key present')
+  }
+
+  var expressions = keys.map(function (key, index) {
+    return decamelize(key) + ' = ' + '$' + (index + 1)
+  })
+
+  return {
+    set: 'SET ' + expressions.join(', '),
+    values: keys.map(function (key) {
+      return safeObject[key]
+    })
+  }
 }
--- test/index.js
+++ test/index.js
@@ -1,21 +1,62 @@
 /* globals describe, it */
+
 var patch = require('../')
 var assert = require('assert')
+var PatchError = require('../errors').PatchError
 
 describe('node-postgres-patch', function () {
+  it('should handle 3 keys using a variable argument list of keys', function () {
+    var output = patch({
+      abstractSyntaxTree: 'jason Murray',
+      antwan: JSON.stringify({ jason: 'Murray Jr' })
+    }, 'abstractSyntaxTree', 'antwan', 'wimberly')
+
+    var expectedOutput = {
+      set: 'SET abstract_syntax_tree = $1, antwan = $2',
+      values: ['jason Murray', '{"jason":"Murray Jr"}']
+    }
+
+    assert.deepEqual(output, expectedOutput)
+  })
+
+  it('should handle 3 keys using an array of keys', function () {
+    var output = patch({
+      wimberly: 39,
+      antwan: JSON.stringify({ jason: 'Murray Jr' }),
+      abstractSyntaxTree: 'jason Murray'
+    }, ['abstractSyntaxTree', 'antwan', 'wimberly'])
+
+    var expectedOutput = {
+      set: 'SET abstract_syntax_tree = $1, antwan = $2, wimberly = $3',
+      values: ['jason Murray', '{"jason":"Murray Jr"}', 39]
+    }
+
+    assert.deepEqual(output, expectedOutput)
+  })
+
   it('should handle 1 key', function () {
-    assert.equal('it was written', patch())
-  })
+    var output = patch({
+      wimberly: 39,
+      antwan: JSON.stringify({ jason: 'Murray Jr' }),
+      massAssignment: 'for push a commit to core rails'
+    }, 'antwan')
 
-  it('should handle 2 keys', function () {
+    var expectedOutput = {
+      set: 'SET antwan = $1',
+      values: ['{"jason":"Murray Jr"}']
+    }
 
-  })
-
-  it('should handle 3 or more keys', function () {
-
+    assert.deepEqual(output, expectedOutput)
   })
 
   it('should throw if an empty object is yielded', function () {
+    var fn = function () {
+      patch({
+        unsafeCode: 39,
+        massAssignment: 'for push a commit to core rails via GitHub attack vector'
+      }, 'antwan')
+    }
 
+    assert.throws(fn, PatchError)
   })
 })
